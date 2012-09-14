35b8d1c2e99417c5b5e88898adb640cabe3c0d18
Tim Staley <tim.staley@soton.ac.uk>
2012-09-14 09:31:16 +0100

    Make no-lus default since there is a FFTW conflict

--- apply_local_lofar_patches.sh
+++ apply_local_lofar_patches.sh
@@ -34,10 +34,10 @@ do
     check_result "LofIm" "git apply $patchfile" $?
 done
 
-cd $LUS_SVNROOT
-for patchfile in $PATCHES_GRP_DIR/lus-patches/*patch
-do
-    echo $patchfile
-    git apply $patchfile
-    check_result "LUS" "git apply $patchfile" $?
-done
+#cd $LUS_SVNROOT
+#for patchfile in $PATCHES_GRP_DIR/lus-patches/*patch
+#do
+#    echo $patchfile
+#    git apply $patchfile
+#    check_result "LUS" "git apply $patchfile" $?
+#done
