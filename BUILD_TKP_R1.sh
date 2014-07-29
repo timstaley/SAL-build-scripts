f625dda1e7d21d38f1ab4c066801a2973176ed22
Tim Staley <tim.staley@astro.ox.ac.uk>
2014-07-29 18:39:06 +0100

    TKP master no longer uses CMake.

--- BUILD_TKP_R1.sh
+++ BUILD_TKP_R1.sh
@@ -2,7 +2,7 @@
 ####################################################################
 BRANCH_TO_BUILD=$1
 if [[ -z "$BRANCH_TO_BUILD" ]]; then
-    BRANCH_TO_BUILD=master
+    BRANCH_TO_BUILD=release1
 fi
 
 startdir=`pwd`
@@ -44,17 +44,29 @@ cp $BUILD_SCRIPTS_DIR/init_scripts/tkp/init-script.sh $TKP_BUILDS_DIR
 #Build and install TKP libs
 
 rm -rf $TKP_SVNROOT/tkp/build 
-cd $TKP_SVNROOT/tkp
+mkdir $TKP_SVNROOT/tkp/build
+cd $TKP_SVNROOT/tkp/build
 
-build_command="python setup.py install
-                 --install-lib=${BUILD_TARGET}/python-packages
-                 --install-scripts=${BUILD_TARGET}/bin
-                 --install-data=${BUILD_TARGET}/python-packages
-"
-echo $build_command > build_command.sh
-bash build_command.sh
-check_result "tkp" "python install" $?
+cmake_command="cmake $TKP_SVNROOT/tkp -DCMAKE_INSTALL_PREFIX=${BUILD_TARGET} 
+		-DTKP_DEVELOP=1 
+		-DPYTHON_PACKAGES_DIR=${BUILD_TARGET}/python-packages"
+echo $cmake_command > cmake_command.sh
+bash cmake_command.sh
+check_result "tkp" "cmake" $?
+
+make -j$LOFAR_BUILD_NJOBS
+check_result "tkp" "build" $?
+make install
+check_result "tkp" "install" $?
  
+#ln -sfnv $BUILD_TARGET $TARGET/tkp-root
+
+#Symlink the catalogs into the installed folder, 
+#so you can easily run a database init:
+#cd $TARGET/tkp-root/database/catalog
+#ln -sfnv $TKP_CATALOGS/NVSS-all_strip.csv nvss/nvss.csv
+#ln -sfnv $TKP_CATALOGS/WENSS-all_strip.csv wenss/wenss.csv
+#ln -sfnv $TKP_CATALOGS/VLSS-all_strip.csv vlss/vlss.csv
 
 #----------------------------------------------------------------------------
 #------------------------------------------------------------------------------
