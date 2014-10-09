e112c95cb34937a8699455914c43a9c2747d2b7f
Tim Staley <tim.staley@astro.ox.ac.uk>
2014-10-09 10:04:23 +0100

    Update LofIm to 2.5.4

--- BUILD_LOFAR_RELEASE.sh
+++ BUILD_LOFAR_RELEASE.sh
@@ -9,7 +9,7 @@ echo "Build scripts are located under $BUILD_SCRIPTS_DIR"
 
 #------------------------------------------------------------------------------
 
-RELEASE_VER=2_2_3
+RELEASE_VER=2_5_4
 DOWNLOAD_RELEASE="t"
 
 TAGPATH=tags/LOFAR-Release-$RELEASE_VER
